88ee8a7f30911c9c51b5b82be0ba86a04ad0f888
Ch3D <xch3dx@gmail.com>
2014-07-31 12:13:21 +0700

    remove email autocompletition

--- Omnom.Linker/app/src/main/java/com/omnom/android/linker/activity/LoginActivity.java
+++ Omnom.Linker/app/src/main/java/com/omnom/android/linker/activity/LoginActivity.java
@@ -1,24 +1,17 @@
 package com.omnom.android.linker.activity;
 
 import android.app.Activity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.provider.ContactsContract;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,7 +20,6 @@ import android.widget.Toast;
 import com.omnom.android.linker.R;
 import com.omnom.android.linker.utils.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -35,14 +27,7 @@ import butterknife.InjectView;
 import butterknife.InjectViews;
 import butterknife.OnClick;
 
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
-
-	private interface ProfileQuery {
-		String[] PROJECTION = {ContactsContract.CommonDataKinds.Email.ADDRESS, ContactsContract.CommonDataKinds.Email.IS_PRIMARY,};
-
-		int ADDRESS = 0;
-		int IS_PRIMARY = 1;
-	}
+public class LoginActivity extends Activity {
 
 	private static class ErrorTextWatcher implements TextWatcher {
 		private TextView view;
@@ -150,7 +135,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 		setContentView(R.layout.activity_login);
 		ButterKnife.inject(this);
 
-		populateAutoComplete();
 		mEditEmail.addTextChangedListener(new ErrorTextWatcher(mTextEmailError));
 		mEditPassword.addTextChangedListener(new ErrorTextWatcher(mTextPasswordError));
 		mEditPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -191,49 +175,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 		final ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 		final NetworkInfo activeNetworkInfo = cm.getActiveNetworkInfo();
 		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-	}
-
-	private void populateAutoComplete() {
-		getLoaderManager().initLoader(0, null, this);
-	}
-
-	@Override
-	public Loader<Cursor> onCreateLoader(int index, Bundle bundle) {
-		return new CursorLoader(this,
-		                        // Retrieve data rows for the device user's 'profile' contact.
-		                        Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-		                                             ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-		                        // Select only email addresses.
-		                        ContactsContract.Contacts.Data.MIMETYPE + " = ?",
-		                        new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE},
-
-		                        // Show primary email addresses first. Note that there won't be
-		                        // a primary email address if the user hasn't specified one.
-		                        ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-	}
-
-	@Override
-	public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-		List<String> emails = new ArrayList<String>();
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast()) {
-			emails.add(cursor.getString(ProfileQuery.ADDRESS));
-			cursor.moveToNext();
-		}
-		addEmailsToAutoComplete(emails);
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-	}
-
-	private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-		ArrayAdapter<String> adapter =
-				new ArrayAdapter<String>(LoginActivity.this, android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-		mEditEmail.setAdapter(adapter);
 	}
 }
 
