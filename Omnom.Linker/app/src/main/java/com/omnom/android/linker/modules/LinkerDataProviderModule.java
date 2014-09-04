3f8918e15a21bcfc3eb215e2e439b1d53063e551
Ch3D <xch3dx@gmail.com>
2014-09-04 19:22:50 +0700

    introduce separate flavors for easier configuration

--- Omnom.Linker/app/src/main/java/com/omnom/android/linker/modules/LinkerDataProviderModule.java
+++ Omnom.Linker/app/src/main/java/com/omnom/android/linker/modules/LinkerDataProviderModule.java
@@ -1,7 +1,9 @@
 package com.omnom.android.linker.modules;
 
+import android.content.Context;
 import android.text.TextUtils;
 
+import com.omnom.android.linker.R;
 import com.omnom.android.linker.activity.LoginActivity;
 import com.omnom.android.linker.api.Protocol;
 import com.omnom.android.linker.api.observable.LinkerObeservableApi;
@@ -18,16 +20,20 @@ import retrofit.RequestInterceptor;
  */
 @Module(injects = {LoginActivity.class}, complete = false, library = true)
 public class LinkerDataProviderModule {
-	public static final String ENDPOINT_DATA_STAND = "http://restaurateur.stand.saintlab.com/";
-	public static final String ENDPOINT_DATA_LAAAAB = "http://restaurateur.laaaab.com";
-
-	public static final String ENDPOINT_AUTH_STAND = "http://wicket.stand.saintlab.com";
-	public static final String ENDPOINT_AUTH_LAAAAB = "http://wicket.laaaab.com";
-
-	private AuthTokenProvider tokenProvider;
-
 	public interface AuthTokenProvider {
 		public String getAuthToken();
+	}
+
+	//	public static final String ENDPOINT_DATA_STAND = "http://restaurateur.stand.saintlab.com/";
+	//	public static final String ENDPOINT_DATA_LAAAAB = "http://restaurateur.laaaab.com";
+	//
+	//	public static final String ENDPOINT_AUTH_STAND = "http://wicket.stand.saintlab.com";
+	//	public static final String ENDPOINT_AUTH_LAAAAB = "http://wicket.laaaab.com";
+	private AuthTokenProvider tokenProvider;
+	private Context mContext;
+
+	public LinkerDataProviderModule(final Context context) {
+		mContext = context;
 	}
 
 	public LinkerDataProviderModule(final AuthTokenProvider tokenProvider) {
@@ -37,14 +43,17 @@ public class LinkerDataProviderModule {
 	@Provides
 	@Singleton
 	LinkerObeservableApi providerLinkerApi() {
-		return LinkerDataProvider.create(ENDPOINT_DATA_STAND, ENDPOINT_AUTH_STAND, new RequestInterceptor() {
-			@Override
-			public void intercept(RequestFacade request) {
-				final String token = tokenProvider.getAuthToken();
-				if(!TextUtils.isEmpty(token)) {
-					request.addHeader(Protocol.HEADER_AUTH_TOKEN, token);
-				}
-			}
-		});
+		return LinkerDataProvider.create(
+				mContext.getString(R.string.config_data_endpoint),
+				mContext.getString(R.string.config_auth_endpoint),
+				new RequestInterceptor() {
+					@Override
+					public void intercept(RequestFacade request) {
+						final String token = tokenProvider.getAuthToken();
+						if(!TextUtils.isEmpty(token)) {
+							request.addHeader(Protocol.HEADER_AUTH_TOKEN, token);
+						}
+					}
+				});
 	}
 }
