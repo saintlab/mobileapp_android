855f20dfbe578d58eda53f5ba7f5ef9a038e92ab
Ch3D <xch3dx@gmail.com>
2014-09-12 14:19:45 +0700

    relayout project structure

---
+++ Omnom.Android/libraries/Omnom.Utils/src/main/java/com/omnom/android/linker/utils/AnimationBuilder.java
@@ -0,0 +1,77 @@
+package com.omnom.android.linker.utils;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+
+import com.omnom.android.utils.R;
+
+/**
+ * Created by Ch3D on 30.07.2014.
+ */
+public class AnimationBuilder {
+	public interface UpdateLisetener {
+		public void invoke(ValueAnimator animation);
+	}
+
+	public static AnimationBuilder create(View view, int... values) {
+		AnimationBuilder animationBuilder = new AnimationBuilder().ofInt(values);
+		animationBuilder.initDefaults(view);
+		return animationBuilder;
+	}
+
+	private ValueAnimator animator;
+
+	private AnimationBuilder() {
+	}
+
+	public void setDuration(long duration) {
+		animator.setDuration(duration);
+	}
+
+	private void initDefaults(View view) {
+		animator.setDuration(view.getResources().getInteger(R.integer.default_animation_duration_long));
+		animator.setInterpolator(new AccelerateDecelerateInterpolator());
+	}
+
+	public AnimationBuilder ofInt(int... values) {
+		animator = ValueAnimator.ofInt(values);
+		return this;
+	}
+
+	public AnimationBuilder addListener(final UpdateLisetener lisetener) {
+		animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+			@Override
+			public void onAnimationUpdate(ValueAnimator animation) {
+				if (lisetener != null) {
+					lisetener.invoke(animation);
+				}
+			}
+		});
+		return this;
+	}
+
+	public AnimationBuilder onEnd(final Runnable action) {
+		animator.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				if (action != null) {
+					action.run();
+				}
+			}
+		});
+		return this;
+	}
+
+	public ValueAnimator build() {
+		return animator;
+	}
+
+	public AnimationBuilder setInterpolator(Interpolator interpolator) {
+		animator.setInterpolator(interpolator);
+		return this;
+	}
+}
---
+++ Omnom.Android/libraries/Omnom.Utils/src/main/java/com/omnom/android/linker/utils/AnimationUtils.java
@@ -0,0 +1,157 @@
+package com.omnom.android.linker.utils;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.omnom.android.utils.R;
+
+/**
+ * Created by Ch3D on 29.07.2014.
+ */
+public class AnimationUtils {
+
+	public static void animateAlpha(final View view, final boolean visible) {
+		animateAlpha(view, visible, view.getResources().getInteger(R.integer.default_animation_duration_short));
+	}
+
+	public static void animateAlpha(final View view, final boolean visible, long duration) {
+		animateAlpha(view, visible, null, duration);
+	}
+
+	public static void animateAlpha(final View view, final boolean visible, final Runnable callback) {
+		animateAlpha(view, visible, callback, view.getResources().getInteger(R.integer.default_animation_duration_short));
+	}
+
+	public static void animateAlpha(final View view, final boolean visible, final Runnable callback, long duration) {
+		final Boolean tag = (Boolean) view.getTag();
+		if(tag != null && tag == visible) {
+			// skip
+			return;
+		}
+
+		view.setAlpha(visible ? 0 : 1);
+		if(visible) {
+			ViewUtils.setVisible(view, visible);
+		}
+		view.setTag(visible);
+		view.animate().setDuration(duration).
+				setInterpolator(new AccelerateDecelerateInterpolator()).
+				setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						if(callback != null) {
+							view.post(callback);
+						}
+					}
+				}).alpha(visible ? 1 : 0).start();
+	}
+
+	public static void animateAlpha2(final View view, final boolean visible, final Runnable callback, long duration) {
+		view.setAlpha(visible ? 0 : 1);
+		if(visible) {
+			ViewUtils.setVisible(view, visible);
+		}
+		view.setTag(visible);
+		view.animate().setDuration(duration).
+				setInterpolator(new AccelerateDecelerateInterpolator()).
+				setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						if(callback != null) {
+							view.post(callback);
+						}
+					}
+				}).alpha(visible ? 1 : 0).start();
+	}
+
+	public static void translateUp(final Iterable<View> views, final int translation, final Runnable endCallback) {
+		final AnimationBuilder builder = AnimationBuilder.create(views.iterator().next(), 0, -translation);
+		prepareTranslation(views, endCallback, builder).start();
+	}
+
+	public static void translateDown(final Iterable<View> views, final int translation, final Runnable endCallback) {
+		final AnimationBuilder builder = AnimationBuilder.create(views.iterator().next(), -translation, 0);
+		prepareTranslation(views, endCallback, builder).start();
+	}
+
+	private static ValueAnimator prepareTranslation(final Iterable<View> views, final Runnable endCallback, AnimationBuilder builder) {
+		return builder.addListener(new AnimationBuilder.UpdateLisetener() {
+			@Override
+			public void invoke(ValueAnimator animation1) {
+				for(View v : views) {
+					v.setTranslationY((Integer) animation1.getAnimatedValue());
+				}
+			}
+		}).onEnd(endCallback).build();
+	}
+
+	public static void scaleWidth(final View view, final int width, final Runnable updateCallback, final Runnable endCallback) {
+		AnimationBuilder builder = AnimationBuilder.create(view, view.getMeasuredWidth(), width);
+		builder.addListener(new AnimationBuilder.UpdateLisetener() {
+			@Override
+			public void invoke(ValueAnimator animation) {
+				view.getLayoutParams().width = (Integer) animation.getAnimatedValue();
+				view.requestLayout();
+				if(updateCallback != null) {
+					view.post(updateCallback);
+				}
+			}
+		});
+		if(endCallback != null) {
+			builder.onEnd(endCallback);
+		}
+		builder.build().start();
+	}
+
+	public static void scaleWidth(final View view, final int width, final long duration,
+	                              final Runnable endCallback) {
+		AnimationBuilder builder = AnimationBuilder.create(view, view.getMeasuredWidth(), width);
+		builder.setDuration(duration);
+		builder.addListener(new AnimationBuilder.UpdateLisetener() {
+			@Override
+			public void invoke(ValueAnimator animation) {
+				view.getLayoutParams().width = (Integer) animation.getAnimatedValue();
+				view.requestLayout();
+			}
+		});
+		if(endCallback != null) {
+			builder.onEnd(endCallback);
+		}
+		builder.build().start();
+	}
+
+	public static void scaleHeight(final View view, int height) {
+		AnimationBuilder.create(view, view.getMeasuredHeight(), height).addListener(new AnimationBuilder.UpdateLisetener() {
+			@Override
+			public void invoke(ValueAnimator animation) {
+				view.getLayoutParams().height = (Integer) animation.getAnimatedValue();
+				view.requestLayout();
+			}
+		}).build().start();
+	}
+
+	public static void scaleHeight(final View view, int height, long duration) {
+		AnimationBuilder builder = AnimationBuilder.create(view, view.getMeasuredHeight(), height);
+		builder.setDuration(duration);
+		builder.addListener(new AnimationBuilder.UpdateLisetener() {
+			@Override
+			public void invoke(ValueAnimator animation) {
+				view.getLayoutParams().height = (Integer) animation.getAnimatedValue();
+				view.requestLayout();
+			}
+		}).build().start();
+	}
+
+	public static void scale(final View view, int size, Runnable endCallback) {
+		scaleHeight(view, size);
+		scaleWidth(view, size, null, endCallback);
+	}
+
+	public static void scale(final View view, int size, long duration, Runnable endCallback) {
+		scaleHeight(view, size, duration);
+		scaleWidth(view, size, duration, endCallback);
+	}
+}
