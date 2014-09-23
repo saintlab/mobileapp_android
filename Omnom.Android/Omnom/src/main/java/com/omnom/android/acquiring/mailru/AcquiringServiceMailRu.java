b8c87d7e6826b8dc79833d851cc7bba01c68e880
Ch3D <xch3dx@gmail.com>
2014-09-23 15:33:29 +0700

    introduce rx observable

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/acquiring/mailru/AcquiringServiceMailRu.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/acquiring/mailru/AcquiringServiceMailRu.java
@@ -1,6 +1,7 @@
 package com.omnom.android.acquiring.mailru;
 
 import com.omnom.android.acquiring.mailru.response.AcquiringResponse;
+import com.omnom.android.acquiring.mailru.response.AcquiringTransactionExtendedResponse;
 import com.omnom.android.acquiring.mailru.response.AcquiringTransactionResponse;
 
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.HashMap;
 import retrofit.http.FieldMap;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.POST;
+import rx.Observable;
 
 /**
  * Created by Ch3D on 23.09.2014.
@@ -15,25 +17,25 @@ import retrofit.http.POST;
 public interface AcquiringServiceMailRu {
 	@FormUrlEncoded
 	@POST("/order/pay")
-	public AcquiringResponse pay(@FieldMap HashMap<String, String> params);
+	public Observable<AcquiringResponse> pay(@FieldMap HashMap<String, String> params);
 
 	@FormUrlEncoded
 	@POST("/card/register")
-	public AcquiringResponse registerCard(@FieldMap HashMap<String, String> params);
+	public Observable<AcquiringResponse> registerCard(@FieldMap HashMap<String, String> params);
 
 	@FormUrlEncoded
 	@POST("/card/verify")
-	public AcquiringResponse verifyCard(@FieldMap HashMap<String, String> params);
+	public Observable<AcquiringResponse> verifyCard(@FieldMap HashMap<String, String> params);
 
 	@FormUrlEncoded
 	@POST("/card/delete")
-	public AcquiringResponse deleteCard(@FieldMap HashMap<String, String> params);
+	public Observable<AcquiringResponse> deleteCard(@FieldMap HashMap<String, String> params);
 
 	@FormUrlEncoded
 	@POST("/transaction/check")
-	public AcquiringTransactionResponse checkTransaction(@FieldMap HashMap<String, String> params);
+	public Observable<AcquiringTransactionResponse> checkTransaction(@FieldMap HashMap<String, String> params);
 
 	@FormUrlEncoded
 	@POST("/transaction/extcheck")
-	public AcquiringTransactionResponse checkTransactionExtended(@FieldMap HashMap<String, String> params);
+	public Observable<AcquiringTransactionExtendedResponse> checkTransactionExtended(@FieldMap HashMap<String, String> params);
 }
