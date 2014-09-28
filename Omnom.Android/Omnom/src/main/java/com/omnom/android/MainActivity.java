c233ddac98344cd2f1fffb86a3bcfaecd230297d
Ch3D <xch3dx@gmail.com>
2014-09-28 16:14:46 +0700

    test mail.ru acquiring

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/MainActivity.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/MainActivity.java
@@ -32,20 +32,34 @@ public class MainActivity extends Activity {
 		OmnomApplication.get(this).inject(this);
 
 		final CardInfo testCard = CardInfo.createTestCard(this);
+//		testCard.setCardId("30008685803965102459");
+//		acquiring.deleteCard(new MerchantData(this), UserData.createTestUser(), testCard, new Acquiring.CardDeleteListener<AcquiringResponse>() {
+//			@Override
+//			public void onCardDeleted(AcquiringResponse response) {
+//				Log.d(TAG, ">>> url = " + response.getUrl());
+//			}
+//		});
 
-		acquiring.registerCard(new MerchantData(this), UserData.createTestUser(), testCard,
-		                       new Acquiring.CardRegisterListener<CardRegisterPollingResponse>() {
-			                       @Override
-			                       public void onCardRegistered(CardRegisterPollingResponse response) {
-				                       Log.d(TAG, "status = " + response.getStatus() + " cardId = " + response.getCardId());
-				                       testCard.setCardId(response.getCardId());
-				                       verifyCard(testCard);
-			                       }
-		                       });
+				acquiring.registerCard(new MerchantData(this), UserData.createTestUser(), testCard,
+				                       new Acquiring.CardRegisterListener<CardRegisterPollingResponse>() {
+					                       @Override
+					                       public void onCardRegistered(CardRegisterPollingResponse response) {
+						                       Log.d(TAG, "status = " + response.getStatus() + " cardId = " + response.getCardId());
+						                       testCard.setCardId(response.getCardId());
+						                       verifyCard(testCard);
+					                       }
+				                       });
+
+//		{
+//			"card_id":"30008685803965102459", "url":
+//			"https:\/\/test-cpg.money.mail.ru\/api\/transaction\/check\/?card_id=30008685803965102459&id=10004247505708511228&signature=169f12bc3bd574a607cf17dc850b0075def10119&status=OK_CHECK"
+//		}
+//		testCard.setCardId("30008685803965102459");
+//		verifyCard(testCard);
 	}
 
 	private void verifyCard(final CardInfo cardInfo) {
-		acquiring.verifyCard(new MerchantData(MainActivity.this), UserData.createTestUser(), cardInfo, 1.20,
+		acquiring.verifyCard(new MerchantData(MainActivity.this), UserData.createTestUser(), cardInfo, 1.4,
 		                     new Acquiring.CardVerifyListener<AcquiringResponse>() {
 			                     @Override
 			                     public void onCardVerified(AcquiringResponse response) {
