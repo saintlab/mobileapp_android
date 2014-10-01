edf77cc63f29a2c3ccd0e5cb9576a67fa2eeaaf3
Ch3D <xch3dx@gmail.com>
2014-10-01 19:00:44 +0700

    add mixpanel dependency and initialization

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/OmnomApplication.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/OmnomApplication.java
@@ -2,6 +2,7 @@ package com.omnom.android;
 
 import android.content.Context;
 
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
 import com.omnom.android.auth.AuthModule;
 import com.omnom.android.modules.AcquiringModuleMailRu;
 import com.omnom.android.modules.AndroidModule;
@@ -22,13 +23,20 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
  * Created by Ch3D on 24.09.2014.
  */
 public class OmnomApplication extends BaseOmnomApplication implements AuthTokenProvider {
+	private static final String MIXPANEL_TOKEN = "36038b10ddd9e23db4a28d0f8c21fd78";
+
 	public static OmnomApplication get(Context context) {
 		return (OmnomApplication) context.getApplicationContext();
+	}
+
+	public static MixpanelAPI getMixPanel(final Context context) {
+		return OmnomApplication.get(context).mixPanel;
 	}
 
 	private final List<Object> injectList = new ArrayList<Object>();
 	private ObjectGraph objectGraph;
 	private PreferenceHelper preferenceHelper;
+	private MixpanelAPI mixPanel;
 
 	protected List<Object> getModules() {
 		return Arrays.asList(new AndroidModule(this), new ApplicationModule(), new AcquiringModuleMailRu(this),
@@ -53,6 +61,8 @@ public class OmnomApplication extends BaseOmnomApplication implements AuthTokenP
 	public void onCreate() {
 		super.onCreate();
 		CalligraphyConfig.initDefault("fonts/Futura-OSF-Omnom-Regular.otf", R.attr.fontPath);
+
+		mixPanel = MixpanelAPI.getInstance(this, MIXPANEL_TOKEN);
 
 		objectGraph = ObjectGraph.create(getModules().toArray());
 		for(final Object obj : injectList) {
