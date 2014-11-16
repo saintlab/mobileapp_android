29f4d5652f2855a61d2c0941cd0d83d25a0e6411
xch3dx <xch3dx@gmail.com>
2014-11-16 15:13:14 +0700

    fix compilation error

--- Omnom.Android/Linker/src/main/java/com/omnom/android/linker/loader/LinkerLoaderError.java
+++ Omnom.Android/Linker/src/main/java/com/omnom/android/linker/loader/LinkerLoaderError.java
@@ -7,16 +7,14 @@ import com.omnom.android.utils.loader.LoaderError;
  * Created by Ch3D on 16.10.2014.
  */
 public class LinkerLoaderError extends LoaderError {
-	public static final LoaderError NO_CONNECTION_BIND = new LinkerLoaderError(R.drawable.ic_no_connection,
-	                                                                           R.string.error_unknown_server_error,
-	                                                                           R.string.bind_table);
+	public static final LoaderError NO_CONNECTION_BIND =
+			new LinkerLoaderError(R.drawable.ic_no_connection, R.string.error_unknown_server_error, R.string.bind_table, 0);
 
+	public static final LoaderError MAINTENANCE_DISABLED =
+			new LinkerLoaderError(R.drawable.ic_maintenance_disabled, R.string.error_maintenance_mode_off, R.string.try_once_again,
+			                      R.drawable.ic_repeat_small);
 
-	public static final LoaderError MAINTENANCE_DISABLED = new LinkerLoaderError(R.drawable.ic_maintenance_disabled,
-	                                                                             R.string.error_maintenance_mode_off,
-	                                                                             R.string.try_once_again);
-
-	public LinkerLoaderError(int drawableResId, int errResId, int btnResId) {
-		super(drawableResId, errResId, btnResId);
+	public LinkerLoaderError(int drawableResId, int errResId, int btnResId, int btnDrawableId) {
+		super(drawableResId, errResId, btnResId, btnDrawableId);
 	}
 }
