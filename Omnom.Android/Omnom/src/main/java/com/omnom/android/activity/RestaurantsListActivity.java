4a0f1fae639a43f38c63025084c78e533bbf8d78
Ch3D <xch3dx@gmail.com>
2014-12-11 12:14:39 +0600

    code cleanup

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/activity/RestaurantsListActivity.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/activity/RestaurantsListActivity.java
@@ -1,5 +1,8 @@
 package com.omnom.android.activity;
 
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +20,9 @@ import com.omnom.android.restaurateur.model.restaurant.Restaurant;
 import com.omnom.android.restaurateur.model.restaurant.RestaurantsResponse;
 import com.omnom.android.utils.utils.AnimationUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.InjectView;
@@ -25,7 +31,7 @@ import rx.functions.Action1;
 
 import static com.omnom.android.utils.utils.AndroidUtils.showToast;
 
-public class RestaurantsListActivity extends BaseOmnomActivity {
+public class RestaurantsListActivity extends BaseOmnomActivity implements AdapterView.OnItemClickListener {
 
 	private static final String TAG = RestaurantsListActivity.class.getSimpleName();
 
@@ -33,17 +39,18 @@ public class RestaurantsListActivity extends BaseOmnomActivity {
 		activity.start(RestaurantsListActivity.class, false);
 	}
 
+	public static void start(BaseOmnomActivity activity, List<Restaurant> restaurants) {
+		final Intent intent = new Intent(activity, RestaurantsListActivity.class);
+		intent.putParcelableArrayListExtra(EXTRA_RESTAURANTS, new ArrayList<Parcelable>(restaurants));
+		activity.start(intent);
+		activity.start(RestaurantsListActivity.class, false);
+	}
+
 	@InjectView(android.R.id.list)
 	protected ListView list;
 
-	@InjectView(R.id.panel_top)
-	protected View panelTop;
-
 	@InjectView(R.id.btn_demo)
 	protected Button btnDemo;
-
-	@InjectView(R.id.img_qr)
-	protected ImageView imgQr;
 
 	@InjectView(R.id.img_profile)
 	protected ImageView imgProfile;
@@ -55,6 +62,9 @@ public class RestaurantsListActivity extends BaseOmnomActivity {
 	RestaurateurObeservableApi api;
 
 	private RestaurantsAdapter mAdapter;
+
+	@Nullable
+	private ArrayList<Restaurant> mRestaurants;
 
 	@OnClick(R.id.img_qr)
 	public void doQrShortcut() {
@@ -75,44 +85,55 @@ public class RestaurantsListActivity extends BaseOmnomActivity {
 	}
 
 	@Override
+	protected void handleIntent(final Intent intent) {
+		mRestaurants = getIntent().getParcelableArrayListExtra(EXTRA_RESTAURANTS);
+		mAdapter = new RestaurantsAdapter(this, mRestaurants);
+	}
+
+	@Override
 	public void initUi() {
 		if(mAdapter == null) {
 			AnimationUtils.animateAlpha(progressBar, true);
+			api.getRestaurants().subscribe(new Action1<RestaurantsResponse>() {
+				@Override
+				public void call(final RestaurantsResponse restaurants) {
+					mAdapter = new RestaurantsAdapter(getActivity(), restaurants.getItems());
+					AnimationUtils.animateAlpha(progressBar, false, new Runnable() {
+						@Override
+						public void run() {
+							initList();
+						}
+					});
+				}
+			}, new OmnomBaseErrorHandler(this) {
+				@Override
+				protected void onThrowable(final Throwable throwable) {
+					AnimationUtils.animateAlpha(progressBar, false);
+					Log.e(TAG, "getRestaurants", throwable);
+				}
+			});
+		} else {
+			initList();
 		}
-		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
-				final Restaurant item = (Restaurant) mAdapter.getItem(position);
-				RestaurantActivity.start(RestaurantsListActivity.this, item);
-				//final ImageView imgCover = (ImageView) view.findViewById(R.id.img_cover);
-				////imgCover.animate().translationYBy(-100).start();
-				////AnimationUtils.scaleHeight(imgCover, 450);
-				//final BitmapDrawable drawable = (BitmapDrawable) imgCover.getDrawable();
-				//final ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat
-				//		.makeThumbnailScaleUpAnimation(view, drawable.getBitmap(), (int)view.getX(), (int)view.getY());
-				//startActivity(new Intent(getActivity(), UserProfileActivity.class), activityOptionsCompat.toBundle());
-			}
-		});
+	}
 
-		api.getRestaurants().subscribe(new Action1<RestaurantsResponse>() {
-			@Override
-			public void call(final RestaurantsResponse restaurants) {
-				mAdapter = new RestaurantsAdapter(getActivity(), restaurants.getItems());
-				AnimationUtils.animateAlpha(progressBar, false, new Runnable() {
-					@Override
-					public void run() {
-						list.addFooterView(LayoutInflater.from(getActivity()).inflate(R.layout.item_restaurants_footer, null));
-						list.setAdapter(mAdapter);
-					}
-				});
-			}
-		}, new OmnomBaseErrorHandler(this) {
-			@Override
-			protected void onThrowable(final Throwable throwable) {
-				AnimationUtils.animateAlpha(progressBar, false);
-				Log.e(TAG, "getRestaurants", throwable);
-			}
-		});
+	private void initList() {
+		list.addFooterView(LayoutInflater.from(getActivity()).inflate(R.layout.item_restaurants_footer, null));
+		list.setAdapter(mAdapter);
+		list.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
+		final Restaurant item = (Restaurant) mAdapter.getItem(position);
+		RestaurantActivity.start(RestaurantsListActivity.this, item);
+		//final ImageView imgCover = (ImageView) view.findViewById(R.id.img_cover);
+		////imgCover.animate().translationYBy(-100).start();
+		////AnimationUtils.scaleHeight(imgCover, 450);
+		//final BitmapDrawable drawable = (BitmapDrawable) imgCover.getDrawable();
+		//final ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat
+		//		.makeThumbnailScaleUpAnimation(view, drawable.getBitmap(), (int)view.getX(), (int)view.getY());
+		//startActivity(new Intent(getActivity(), UserProfileActivity.class), activityOptionsCompat.toBundle());
 	}
 
 	@Override
