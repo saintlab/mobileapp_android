6dfb00c094e25b5c22c179c22a36de422ae84a6b
Ch3D <xch3dx@gmail.com>
2014-12-15 14:26:24 +0600

    fix QR related protocol : QR -> List<QR>

--- Omnom.Android/libraries/Omnom.Restaurateur/src/main/java/com/omnom/android/restaurateur/model/table/TableDataResponse.java
+++ Omnom.Android/libraries/Omnom.Restaurateur/src/main/java/com/omnom/android/restaurateur/model/table/TableDataResponse.java
@@ -12,23 +12,25 @@ import java.util.List;
  * Created by Ch3D on 26.08.2014.
  */
 public class TableDataResponse extends ResponseBase {
-	public static final TableDataResponse NULL = new TableDataResponse("null_id", 0, "null_qr_data", "null_rest_id");
+	public static final TableDataResponse NULL = new TableDataResponse("null_id", 0, (List<String>) Collections.EMPTY_LIST,
+	                                                                   "null_rest_id");
 
 	@Expose
 	private int internalId;
 
 	@Expose
-	private String qrCode;
+	private List<String> qrCode = new ArrayList<String>();
 
 	@Expose
 	private String restaurantId;
 
 	@Expose
 	private String id;
+
 	@Expose
 	private List<String> beaconUuids = new ArrayList<String>();
 
-	public TableDataResponse(String id, int internalId, String qrCode, String restaurantId) {
+	public TableDataResponse(String id, int internalId, List<String> qrCode, String restaurantId) {
 		this.internalId = internalId;
 		this.qrCode = qrCode;
 		this.restaurantId = restaurantId;
@@ -44,7 +46,7 @@ public class TableDataResponse extends ResponseBase {
 		return internalId;
 	}
 
-	public String getQrCode() {
+	public List<String> getQrCode() {
 		return qrCode;
 	}
 
