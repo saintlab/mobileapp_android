c923eab2e759e9efec9888a91e4c9fd00dd4f97e
Ch3D <xch3dx@gmail.com>
2015-01-03 15:31:47 +0600

    divide ble into separated activities

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/activity/ValidateActivityBle21.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/activity/ValidateActivityBle21.java
@@ -6,6 +6,7 @@ import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Build;
 import android.util.Log;
 
@@ -27,12 +28,20 @@ public class ValidateActivityBle21 extends ValidateActivityBle {
 
 	private ScanCallback mScanCallback;
 
+	private ScanSettings mScanSettings;
+
 	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 	@Override
 	protected void initBle() {
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		parser = new BeaconParser();
 		parser.setBeaconLayout(getResources().getString(R.string.redbear_beacon_layout));
+
+		ScanSettings.Builder builder = new ScanSettings.Builder();
+		builder.setReportDelay(0);
+		builder.setScanMode(ScanSettings.SCAN_MODE_BALANCED);
+		mScanSettings = builder.build();
+
 		mScanCallback = new ScanCallback() {
 			@Override
 			public void onScanResult(final int callbackType, final ScanResult result) {
@@ -85,7 +94,6 @@ public class ValidateActivityBle21 extends ValidateActivityBle {
 	@Override
 	protected void scanBleDevices(final boolean enable, final Runnable endCallback) {
 		final BluetoothLeScanner scanner = mBluetoothAdapter.getBluetoothLeScanner();
-
 		if(enable) {
 			mBeacons.clear();
 			findViewById(android.R.id.content).postDelayed(new Runnable() {
@@ -97,7 +105,7 @@ public class ValidateActivityBle21 extends ValidateActivityBle {
 					}
 				}
 			}, getResources().getInteger(R.integer.ble_scan_duration));
-			scanner.startScan(mScanCallback);
+			scanner.startScan(null, mScanSettings, mScanCallback);
 		} else {
 			scanner.stopScan(mScanCallback);
 			if(endCallback != null) {
