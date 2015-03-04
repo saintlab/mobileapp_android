c1a89a785e175706e8743b0ec9347fed67d9f513
Ch3D <xch3dx@gmail.com>
2015-03-04 19:35:30 +0600

    handle new order push

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/service/bluetooth/PushWooshService.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/service/bluetooth/PushWooshService.java
@@ -2,14 +2,21 @@ package com.omnom.android.service.bluetooth;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.arellomobile.android.push.DeviceRegistrar;
 import com.arellomobile.android.push.PushGCMIntentService;
+import com.google.gson.Gson;
 import com.omnom.android.R;
+import com.omnom.android.utils.utils.StringUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Ch3D on 28.02.2015.
@@ -17,6 +24,14 @@ import com.omnom.android.R;
 public class PushWooshService extends PushGCMIntentService {
 
 	private static final String TAG = PushWooshService.class.getSimpleName();
+
+	private Gson mGson;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mGson = new Gson();
+	}
 
 	@Override
 	protected void onRegistered(final Context context, final String registrationId) {
@@ -37,14 +52,29 @@ public class PushWooshService extends PushGCMIntentService {
 	}
 
 	private void showNotification(final Context context, final Intent intent) {
+		String title = StringUtils.EMPTY_STRING;
+		try {
+			final JSONObject json = new JSONObject(intent.getStringExtra("aps"));
+			title = json.optString("alert", "Приятного аппетита!");
+		} catch(JSONException e) {
+			e.printStackTrace();
+		}
+		final String hash = intent.getStringExtra("hash");
+		final String type = intent.getStringExtra("type");
+
 		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+		final Intent hashIntent = new Intent(Intent.ACTION_VIEW);
+		hashIntent.addCategory(Intent.CATEGORY_DEFAULT);
+		hashIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+		hashIntent.setData(Uri.parse(hash));
+
 		final Notification notification = builder.setAutoCancel(true)
-		                                         .setContentTitle("TITLE")
-		                                         .setContentInfo("INFO")
+		                                         .setContentTitle(title)
 		                                         .setSmallIcon(R.drawable.ic_app)
 		                                         .setDefaults(Notification.DEFAULT_ALL)
-		                                         .setTicker("TICKER")
+		                                         .setTicker(title)
+		                                         .setContentIntent(PendingIntent.getActivity(context, 0, hashIntent, 0))
 		                                         .build();
 		nm.notify(0, notification);
 	}
--- Omnom.Android/libraries/Omnom.Notifier/src/main/java/com/omnom/android/notifier/api/NotifierDataService.java
+++ Omnom.Android/libraries/Omnom.Notifier/src/main/java/com/omnom/android/notifier/api/NotifierDataService.java
@@ -4,7 +4,6 @@ import com.omnom.android.notifier.model.RegisterRequest;
 import com.omnom.android.protocol.Protocol;
 
 import retrofit.http.Body;
-import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
 import rx.Observable;
@@ -17,10 +16,10 @@ public interface NotifierDataService {
 	@POST("/notifier/register")
 	public Observable<Object> register(@Body RegisterRequest request);
 
-	@GET("/restaurants/{restaurant_id}/tables/{table_id}/in")
+	@POST("/restaurants/{restaurant_id}/tables/{table_id}/in")
 	public Observable<Object> tableIn(@Path(Protocol.FIELD_RESTAURANT_ID) String restId, @Path(Protocol.FIELD_TABLE_ID) String tableId);
 
-	@GET("/restaurants/{restaurant_id}/tables/{table_id}/out")
+	@POST("/restaurants/{restaurant_id}/tables/{table_id}/out")
 	public Observable<Object> tableOut(@Path(Protocol.FIELD_RESTAURANT_ID) String restId, @Path(Protocol.FIELD_TABLE_ID) String tableId);
 
 	@POST("/notifier/unregister")
