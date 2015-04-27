02c061b302079809f9a4e4bce1f64ee6888d5c35
Dmitry Chertenko <xch3dx@gmail.com>
2015-04-27 21:10:24 +0600

    introduce OrderResultActivity

---
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/activity/OrderResultActivity.java
@@ -0,0 +1,18 @@
+package com.omnom.android.activity;
+
+import com.omnom.android.activity.base.BaseOmnomActivity;
+
+/**
+ * Created by xCh3Dx on 27.04.2015.
+ */
+public class OrderResultActivity extends BaseOmnomActivity {
+    @Override
+    public void initUi() {
+
+    }
+
+    @Override
+    public int getLayoutResource() {
+        return 0;
+    }
+}
