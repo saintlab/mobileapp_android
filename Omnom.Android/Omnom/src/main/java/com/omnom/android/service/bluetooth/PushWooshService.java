2c3c6600934d3c3075c9dd188d9bb5e57d201274
Ch3D <xch3dx@gmail.com>
2015-05-07 14:49:58 +0600

    implement push handling for https://github.com/saintlab/mobileapp_android/issues/425 and https://github.com/saintlab/mobileapp_android/issues/424

--- Omnom.Android/Omnom/src/main/java/com/omnom/android/service/bluetooth/PushWooshService.java
+++ Omnom.Android/Omnom/src/main/java/com/omnom/android/service/bluetooth/PushWooshService.java
@@ -1,5 +1,14 @@
 package com.omnom.android.service.bluetooth;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.arellomobile.android.push.DeviceRegistrar;
 import com.arellomobile.android.push.PushGCMIntentService;
 import com.omnom.android.BuildConfig;
@@ -13,178 +22,175 @@ import com.omnom.android.utils.utils.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-
 /**
  * Created by Ch3D on 28.02.2015.
  */
 public class PushWooshService extends PushGCMIntentService {
 
-    private static boolean LOG_ENABLED = BuildConfig.DEBUG;
+	public static final String ARG_ANDROID_PAYLOAD = "aps";
 
-    public static final String ARG_ANDROID_PAYLOAD = "aps";
+	public static final String ARG_HASH = "hash";
 
-    public static final String ARG_HASH = "hash";
+	public static final String ARG_ALERT = "alert";
 
-    public static final String ARG_ALERT = "alert";
+	public static final String ARG_BODY = "body";
 
-    public static final String ARG_BODY = "body";
+	public static final String ARG_WISH_URL = "url";
 
-    public static final String EVENT_PUSH_RECEIVED = "PUSH_RECEIVED";
+	public static final String EVENT_PUSH_RECEIVED = "PUSH_RECEIVED";
 
-    private static final String TAG = PushWooshService.class.getSimpleName();
+	private static final String TAG = PushWooshService.class.getSimpleName();
 
-    @Override
-    protected void onRegistered(final Context context, final String registrationId) {
-        if (LOG_ENABLED) {
-            Log.d(TAG, "onRegistered : " + registrationId);
-        }
-        DeviceRegistrar.registerWithServer(context, registrationId);
-    }
+	private static boolean LOG_ENABLED = BuildConfig.DEBUG;
 
-    @Override
-    protected void onUnregistered(final Context context, final String registrationId) {
-        if (LOG_ENABLED) {
-            Log.d(TAG, "onUnregistered : " + registrationId);
-        }
-        DeviceRegistrar.unregisterWithServer(context, registrationId);
-    }
+	@Override
+	protected void onRegistered(final Context context, final String registrationId) {
+		if(LOG_ENABLED) {
+			Log.d(TAG, "onRegistered : " + registrationId);
+		}
+		DeviceRegistrar.registerWithServer(context, registrationId);
+	}
 
-    @Override
-    protected void onMessage(final Context context, final Intent intent) {
-        if (LOG_ENABLED) {
-            Log.d(TAG, "onMessage : " + intent);
-        }
-        if (Boolean.TRUE.toString().equals(intent.getStringExtra("show_table_orders"))) {
-            showNotification(context, intent);
-        } else {
-            showWishNotification(context, intent);
-        }
-    }
+	@Override
+	protected void onUnregistered(final Context context, final String registrationId) {
+		if(LOG_ENABLED) {
+			Log.d(TAG, "onUnregistered : " + registrationId);
+		}
+		DeviceRegistrar.unregisterWithServer(context, registrationId);
+	}
 
-    private void showWishNotification(final Context context, final Intent intent) {
-        String info = getString(R.string.notification_tap_to_open_omnom);
+	@Override
+	protected void onMessage(final Context context, final Intent intent) {
+		if(LOG_ENABLED) {
+			Log.d(TAG, "onMessage : " + intent);
+		}
+		if(Boolean.TRUE.toString().equals(intent.getStringExtra("show_table_orders"))) {
+			showNotification(context, intent);
+		} else {
+			showWishNotification(context, intent);
+		}
+	}
 
-        try {
-            final JSONObject json = new JSONObject(intent.getStringExtra(ARG_ANDROID_PAYLOAD));
-            final JSONObject objAlert = json.getJSONObject(ARG_ALERT);
-            info = objAlert.optString(ARG_BODY, info);
-        } catch (JSONException e) {
-            Log.e(TAG, "showWishNotification", e);
-        }
+	private void showWishNotification(final Context context, final Intent dataIntent) {
+		String info = getString(R.string.notification_tap_to_open_omnom);
+		Intent actionIntent = getEnteringIntent(context);
 
-        notify(context, getString(R.string.app_name), info, getEnteringIntent(context));
-    }
+		try {
+			final JSONObject json = new JSONObject(dataIntent.getStringExtra(ARG_ANDROID_PAYLOAD));
+			final JSONObject objAlert = json.getJSONObject(ARG_ALERT);
+			info = objAlert.optString(ARG_BODY, info);
+			// TODO: Исправить имя аргумента на реальное
+			final String url = objAlert.optString(ARG_WISH_URL, StringUtils.EMPTY_STRING);
+			actionIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		} catch(JSONException e) {
+			Log.e(TAG, "showWishNotification", e);
+		}
 
-    private void notify(Context context, String title, String info, Intent intent) {
-        reportPushReceived(context, intent);
+		notify(context, getString(R.string.app_name), info, actionIntent);
+	}
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        final Notification notification = builder.setAutoCancel(true)
-                .setContentTitle(title)
-                .setContentText(info)
-                .setSmallIcon(R.drawable.ic_app)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setTicker(title)
-                .setContentIntent(PendingIntent.getActivity(context, 0, intent, 0))
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(info))
-                .build();
+	private void notify(Context context, String title, String info, Intent intent) {
+		reportPushReceived(context, intent);
 
-        NotificationManager nm = (NotificationManager) context
-                .getSystemService(NOTIFICATION_SERVICE);
-        nm.notify(0, notification);
-    }
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+		final Notification notification = builder.setAutoCancel(true)
+		                                         .setContentTitle(title)
+		                                         .setContentText(info)
+		                                         .setSmallIcon(R.drawable.ic_app)
+		                                         .setDefaults(Notification.DEFAULT_ALL)
+		                                         .setTicker(title)
+		                                         .setContentIntent(PendingIntent.getActivity(context, 0, intent, 0))
+		                                         .setStyle(new NotificationCompat.BigTextStyle().bigText(info))
+		                                         .build();
 
-    private void showNotification(final Context context, final Intent intent) {
-        String title = getString(R.string.app_name);
-        String info = getString(R.string.notification_tap_to_open_omnom);
+		NotificationManager nm = (NotificationManager) context
+				.getSystemService(NOTIFICATION_SERVICE);
+		nm.notify(0, notification);
+	}
 
-        try {
-            final JSONObject json = new JSONObject(intent.getStringExtra(ARG_ANDROID_PAYLOAD));
-            title = json.optString(ARG_ALERT);
-            final String[] split = title.split(StringUtils.NEXT_STRING);
-            if (split.length > 0) {
-                title = split[0];
-            }
-            if (split.length > 1) {
-                info = split[1];
-            }
-        } catch (JSONException e) {
-            Log.e(TAG, "showNotification", e);
-        }
+	private void showNotification(final Context context, final Intent intent) {
+		String title = getString(R.string.app_name);
+		String info = getString(R.string.notification_tap_to_open_omnom);
 
-        final String hash = intent.getStringExtra(ARG_HASH);
-        notify(context, title, info, getEnteringIntent(context).setData(Uri.parse(hash)));
-    }
+		try {
+			final JSONObject json = new JSONObject(intent.getStringExtra(ARG_ANDROID_PAYLOAD));
+			title = json.optString(ARG_ALERT);
+			final String[] split = title.split(StringUtils.NEXT_STRING);
+			if(split.length > 0) {
+				title = split[0];
+			}
+			if(split.length > 1) {
+				info = split[1];
+			}
+		} catch(JSONException e) {
+			Log.e(TAG, "showNotification", e);
+		}
 
-    private Intent getEnteringIntent(Context context) {
-        final Intent hashIntent = new Intent(Intent.ACTION_MAIN);
-        hashIntent.setClass(context, EnteringActivity.class);
-        hashIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        hashIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        return hashIntent;
-    }
+		final String hash = intent.getStringExtra(ARG_HASH);
+		notify(context, title, info, getEnteringIntent(context).setData(Uri.parse(hash)));
+	}
 
-    @Deprecated
-    private void showWebNotification(final Context context, final Intent data) {
-        NotificationManager nm = (NotificationManager) context
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+	private Intent getEnteringIntent(Context context) {
+		final Intent hashIntent = new Intent(Intent.ACTION_MAIN);
+		hashIntent.setClass(context, EnteringActivity.class);
+		hashIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+		hashIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		return hashIntent;
+	}
 
-        final PendingIntent activityIntent = PendingIntent.getActivity(context,
-                0,
-                WebActivity.createIntent(context, "http://www.android.com/"),
-                0);
-        final Notification notification = builder.setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_app)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setTicker("Ваш заказ готов")
-                .setContentTitle("Ваш заказ готов")
-                .setContentIntent(activityIntent)
-                .setContentInfo("Нажмите, чтобы посмотреть детали")
-                .build();
-        nm.notify(0, notification);
-    }
+	@Deprecated
+	private void showWebNotification(final Context context, final Intent data) {
+		NotificationManager nm = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
-    private void reportPushReceived(final Context context, final Intent intent) {
-        if (intent != null) {
-            OmnomApplication.getMixPanelHelper(context).track(MixPanelHelper.Project.OMNOM_ANDROID,
-                    EVENT_PUSH_RECEIVED,
-                    new Object[]{
-                            intent.getStringExtra(ARG_ANDROID_PAYLOAD),
-                            intent.getStringExtra(ARG_HASH)
-                    });
-        } else {
-            OmnomApplication.getMixPanelHelper(context).track(MixPanelHelper.Project.OMNOM_ANDROID,
-                    EVENT_PUSH_RECEIVED,
-                    "Intent is empty");
-        }
-    }
+		final PendingIntent activityIntent = PendingIntent.getActivity(context,
+		                                                               0,
+		                                                               WebActivity.createIntent(context, "http://www.android.com/"),
+		                                                               0);
+		final Notification notification = builder.setAutoCancel(true)
+		                                         .setSmallIcon(R.drawable.ic_app)
+		                                         .setDefaults(Notification.DEFAULT_ALL)
+		                                         .setTicker("Ваш заказ готов")
+		                                         .setContentTitle("Ваш заказ готов")
+		                                         .setContentIntent(activityIntent)
+		                                         .setContentInfo("Нажмите, чтобы посмотреть детали")
+		                                         .build();
+		nm.notify(0, notification);
+	}
 
-    @Override
-    protected void onDeletedMessages(final Context context, final String s) {
-        if (LOG_ENABLED) {
-            Log.d(TAG, "onDeletedMessages : " + s);
-        }
-    }
+	private void reportPushReceived(final Context context, final Intent intent) {
+		if(intent != null) {
+			OmnomApplication.getMixPanelHelper(context).track(MixPanelHelper.Project.OMNOM_ANDROID,
+			                                                  EVENT_PUSH_RECEIVED,
+			                                                  new Object[]{
+					                                                  intent.getStringExtra(ARG_ANDROID_PAYLOAD),
+					                                                  intent.getStringExtra(ARG_HASH)
+			                                                  });
+		} else {
+			OmnomApplication.getMixPanelHelper(context).track(MixPanelHelper.Project.OMNOM_ANDROID,
+			                                                  EVENT_PUSH_RECEIVED,
+			                                                  "Intent is empty");
+		}
+	}
 
-    @Override
-    protected void onError(final Context context, final String s) {
-        Log.d(TAG, "onError : " + s);
-    }
+	@Override
+	protected void onDeletedMessages(final Context context, final String s) {
+		if(LOG_ENABLED) {
+			Log.d(TAG, "onDeletedMessages : " + s);
+		}
+	}
 
-    @Override
-    protected boolean onRecoverableError(final Context context, final String s) {
-        if (LOG_ENABLED) {
-            Log.d(TAG, "onRecoverableError : " + s);
-        }
-        return super.onRecoverableError(context, s);
-    }
+	@Override
+	protected void onError(final Context context, final String s) {
+		Log.d(TAG, "onError : " + s);
+	}
+
+	@Override
+	protected boolean onRecoverableError(final Context context, final String s) {
+		if(LOG_ENABLED) {
+			Log.d(TAG, "onRecoverableError : " + s);
+		}
+		return super.onRecoverableError(context, s);
+	}
 }
