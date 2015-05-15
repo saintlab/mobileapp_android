0083a3c6f4f7f16c155cdd9eeb9cff96d9c417a4
Ch3D <xch3dx@gmail.com>
2015-05-15 19:07:20 +0600

    double-check restaurant settings

--- Omnom.Android/libraries/Omnom.Restaurateur/src/main/java/com/omnom/android/entrance/EntranceDataFactory.java
+++ Omnom.Android/libraries/Omnom.Restaurateur/src/main/java/com/omnom/android/entrance/EntranceDataFactory.java
@@ -2,18 +2,28 @@ package com.omnom.android.entrance;
 
 import com.omnom.android.restaurateur.model.restaurant.Restaurant;
 import com.omnom.android.restaurateur.model.restaurant.RestaurantHelper;
+import com.omnom.android.restaurateur.model.restaurant.Settings;
 
 /**
  * Created by Ch3D on 13.05.2015.
  */
 public class EntranceDataFactory {
 	public static EntranceData create(Restaurant restaurant) {
-		if(RestaurantHelper.isBar(restaurant)) {
+		if(restaurant == null || restaurant.settings() == null) {
+			return TableEntranceData.create();
+		}
+
+		final Settings settings = restaurant.settings();
+		final boolean hasBar = settings.hasBar();
+		final boolean hasNonBar = settings.hasPreOrder() || settings.hasTableOrder();
+		if(RestaurantHelper.isBar(restaurant) || (hasBar && !hasNonBar)) {
 			return BarEntranceData.create();
 		}
+
 		if(RestaurantHelper.isTakeAway(restaurant)) {
 			return TakeawayEntranceData.create();
 		}
+
 		return TableEntranceData.create();
 	}
 }
