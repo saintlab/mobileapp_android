4ef9e0013f43b4bae621cfe5e0a64f2836c7f0f8
Ch3D <xch3dx@gmail.com>
2015-06-11 16:21:56 +0600

    fix adapter manipulations

--- Omnom.Android/Omnom/src/androidTest/java/com/omnom/android/debug/test/RestaurantsListActivityTest.java
+++ Omnom.Android/Omnom/src/androidTest/java/com/omnom/android/debug/test/RestaurantsListActivityTest.java
@@ -2,13 +2,19 @@ package com.omnom.android.debug.test;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.HeaderViewListAdapter;
 import android.widget.ListView;
 
+import com.nhaarman.listviewanimations.appearance.simple.SwingBottomInAnimationAdapter;
 import com.omnom.android.R;
 import com.omnom.android.activity.RestaurantActivity;
 import com.omnom.android.activity.RestaurantsListActivity;
+import com.omnom.android.activity.ValidateActivityBle18;
+import com.omnom.android.activity.ValidateActivityBle21;
 import com.omnom.android.adapter.RestaurantsAdapter;
 import com.omnom.android.restaurateur.model.restaurant.Restaurant;
+import com.omnom.android.utils.utils.AndroidUtils;
+import com.omnom.android.utils.utils.BluetoothUtils;
 import com.omnom.android.utils.utils.ViewUtils;
 
 import junit.framework.Assert;
@@ -51,17 +57,72 @@ public class RestaurantsListActivityTest extends BaseOmnomActivityTestCase<Resta
 	public void testLoadRestaurantsListData() {
 		try {
 			passThroughActivityCreateLifecycle();
+
 			waitNetworkData();
 
 			runTestOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					Assert.assertTrue(mList.getAdapter().getCount() > 0);
+					final ListView listView = (ListView) solo.getView(android.R.id.list);
+					Assert.assertTrue(getRestaurantsAdapter(listView).getCount() > 0);
 				}
 			});
 		} catch(Throwable throwable) {
+			Assert.fail(throwable.getMessage());
 			Log.e(TAG, "testLoadRestaurantsListData", throwable);
 		}
+	}
+
+	public void testRestaurantBarLanding() {
+		try {
+			passThroughActivityCreateLifecycle();
+
+			waitNetworkData();
+
+			runTestOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					final ListView listView = (ListView) solo.getView(android.R.id.list);
+					final RestaurantsAdapter adapter = getRestaurantsAdapter(listView);
+
+					Restaurant restaurant = null;
+					int position = -1;
+					for(int i = 0; i < adapter.getCount(); i++) {
+						final Restaurant item = (Restaurant) adapter.getItem(i);
+						if(item.isBar()) {
+							restaurant = item;
+							position = i;
+							break;
+						}
+					}
+
+					if(restaurant == null) {
+						// skip - there is no suitable restaurant
+						return;
+					}
+					listView.performItemClick(listView, position, listView.getItemIdAtPosition(position));
+				}
+			});
+
+			sleepTransition();
+
+			if(BluetoothUtils.hasBleSupport(getActivity()) && BluetoothUtils.isBluetoothEnabled(getActivity())) {
+				if(AndroidUtils.isLollipop()) {
+					assertCurrentActivity(ValidateActivityBle21.class);
+				} else {
+					assertCurrentActivity(ValidateActivityBle18.class);
+				}
+			}
+		} catch(Throwable throwable) {
+			Assert.fail(throwable.getMessage());
+			Log.e(TAG, "testRestaurantDetailsOpening", throwable);
+		}
+	}
+
+	private RestaurantsAdapter getRestaurantsAdapter(final ListView listView) {
+		final HeaderViewListAdapter headerViewListAdapter = (HeaderViewListAdapter) listView.getAdapter();
+		final SwingBottomInAnimationAdapter wrappedAdapter = (SwingBottomInAnimationAdapter) headerViewListAdapter.getWrappedAdapter();
+		return (RestaurantsAdapter) wrappedAdapter.getDecoratedBaseAdapter();
 	}
 
 	public void testRestaurantDetailsOpening() {
@@ -70,17 +131,27 @@ public class RestaurantsListActivityTest extends BaseOmnomActivityTestCase<Resta
 
 			waitNetworkData();
 
-			final boolean[] bar = new boolean[1];
-
 			runTestOnUiThread(new Runnable() {
 				@Override
 				public void run() {
 					final ListView listView = (ListView) solo.getView(android.R.id.list);
-					final RestaurantsAdapter adapter = (RestaurantsAdapter) listView.getAdapter();
+					final RestaurantsAdapter adapter = getRestaurantsAdapter(listView);
 
-					final int position = 1;
-					final Restaurant item = (Restaurant) adapter.getItem(position);
-					bar[0] = item.isBar();
+					Restaurant restaurant = null;
+					int position = -1;
+					for(int i = 0; i < adapter.getCount(); i++) {
+						final Restaurant item = (Restaurant) adapter.getItem(i);
+						if(!item.isBar() && !item.isLunch() && !item.isTakeAway()) {
+							restaurant = item;
+							position = i;
+							break;
+						}
+					}
+
+					if(restaurant == null) {
+						// skip - there is no suitable restaurant
+						return;
+					}
 					listView.performItemClick(listView, position, listView.getItemIdAtPosition(position));
 				}
 			});
@@ -95,6 +166,7 @@ public class RestaurantsListActivityTest extends BaseOmnomActivityTestCase<Resta
 
 			assertCurrentActivity(RestaurantsListActivity.class);
 		} catch(Throwable throwable) {
+			Assert.fail(throwable.getMessage());
 			Log.e(TAG, "testRestaurantDetailsOpening", throwable);
 		}
 	}
